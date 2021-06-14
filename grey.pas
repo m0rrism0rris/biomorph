424a25e18bc0a071c9cafddad58e5062836a2580
m0rrism0rris <75397647+m0rrism0rris@users.noreply.github.com>
2021-06-14 14:06:34 -0400

    Update grey.pas

--- grey.pas
+++ grey.pas
@@ -7,19 +7,11 @@ const
 
 	MinReal = -2.0;
 	MaxReal = 2.0;
-	SclReal = 4.0 / 2048.0; (* (MaxReal - MinReal) / ScreenWidth  *)
+	SclReal = 4.0 / 2048.0; (* (MaxReal - MinReal) / ScreenWidth *)
 	
 	MinImag = -2.0;
 	MaxImag = 2.0;
-	SclImag = 4.0 / 2048.0; (* (MaxReal - MinReal) / ScreenWidth  *)
-
-//	C: complex = (Re: 0.0; Im: 0.0);
-
-function CCube (Z: complex): complex; inline;
-begin
-	CCube.Re := Z.Re * Z.Re * Z.Re - 3 * Z.Re * Z.Im * Z.Im;
-	CCube.Im := 3 * Z.Re * Z.Re * Z.Im - Z.Im * Z.Im * Z.Im 
-end;
+	SclImag = 4.0 / 2048.0; (* (MaxReal - MinReal) / ScreenWidth *)
 
 function FZ (Z: complex): complex; inline;
 const
@@ -28,8 +20,7 @@ begin
 	FZ := CSin (Z) + CSqr(Z) + C;
 end;
 
-{$PUSH} {$BOOLEVAL OFF}
-function BiomorphA (X, Y: nativeUInt): byte;
+function Biomorph (X, Y: nativeUInt): byte;
 var
 	Z: complex;
 	Iter: nativeUInt;
@@ -43,11 +34,11 @@ begin
 	Z.Re := Abs (Z.Re);
 	Z.Im := Abs (Z.Im);
 	if Z.Re < 10.0 then
-		BiomorphA := 127 + Trunc (Z.Re * 12.8)
+		Biomorph := 127 + Trunc (Z.Re * 12.8)
 	else if Z.Im < 10.0 then
-		BiomorphA := 128 - Trunc (Z.Im * 12.8)
+		Biomorph := 128 - Trunc (Z.Im * 12.8)
 	else
-		BiomorphA := 127;
+		Biomorph := 127;
 end;
 
 var
@@ -56,33 +47,12 @@ var
 	Ch: char;
 
 begin
-	Assign (OutFile, '/Users/cy4n/Desktop/frac/biomorph.pgm');
+	Assign (OutFile, 'biomorph.pgm');
 	Rewrite (OutFile);
 	for Ch in 'P5 ' + IntToStr (PixlWidth) + ' ' + IntToStr (PixlHeight) + ' 255' + LineEnding do Write (OutFile, Ord (Ch));
 	for Y := 1 to PixlHeight do begin
 		Write (#13, Y);
-		for X := 1 to PixlWidth do Write (OutFile, BiomorphA (X, Y));
+		for X := 1 to PixlWidth do Write (OutFile, Biomorph (X, Y));
 	end;
 	WriteLn;
 end.
-{$POP}
-
-(*
-int const
-_ ScreenHeight = 63,
-_ ScreenWidth = 47
-float const
-_ MinReal := -2.0,
-_ MaxReal := 2.0,
-_ MinImag := -2.0,
-_ MaxImag := 2.0
-float const
-_ SclReal := (MaxReal - MinReal) / ScreenWidth,
-_ SclImag := (MaxImag - MinImag) / ScreenHeight
-
-int function BiomorphA (int X, Y) is
-	complex var Z
-	
-;
-
-*)
